b0f424ccdea7e20404fe8c723d4dc6b8294a9ff8
snigdha <snigdhask27@gmail.com>
2026-04-22 11:15:03 +0530

    Framework updated

--- src/main/java/com/ExpandTesting/Files/PageTitles.java
+++ src/main/java/com/ExpandTesting/Files/PageTitles.java
@@ -15,6 +15,7 @@ public class PageTitles {
  public static final String OtpTitle = "OTP Login page for Automation Testing Practice";
  public static final String OtpEmail = "practice@expandtesting.com";
  public static final String OtpSuccessMsg = "You logged into a secure area!";
+ 
  //public static final String OtpInvalidMsg = "Invalid OTP code.";
  public static final String OtpInvalidMsg = "The provided OTP code is incorrect. Please check your code and try again.";
  public static final String OtpEmptyMsg = "The OTP code is required.";
